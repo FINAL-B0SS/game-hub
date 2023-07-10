85bd23551cee80c1055e8f761add0a3ca420b967
Malik X. Jean <1malik.jean@gmail.com>
2023-07-09 22:58:32 -0700

    Adds color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { ColorModeScript, HStack, Image, Text } from "@chakra-ui/react";
 import logo from '../assets/smile.png'
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
-  return <HStack>
-    <Image src={logo} />
-    <Text>NavBar</Text>
+  return <HStack justifyContent="space-between" padding="10px">
+    <Image src={logo} boxSize={50}/>
+    <ColorModeSwitch />
   </HStack>;
 };
 
